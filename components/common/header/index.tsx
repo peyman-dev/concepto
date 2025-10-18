5865ffe3b819e1d7c99b26f63e74760c8a414636
Peyman Ahmadi <peymangeek@gmail.com>
2025-10-18 11:57:56 +0330

    Components moved to root folder

---
+++ components/common/header/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Desktop from './fragments/desktop'
+import Mobile from './fragments/mobile'
+
+const Header = () => {
+  return (
+    <header>
+            <Desktop />
+            <Mobile />
+    </header>
+  )
+}
+
+export default Header
