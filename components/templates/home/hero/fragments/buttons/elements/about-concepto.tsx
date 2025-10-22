27218f1d7856396e4d19619901406af800fa5477
Peyman Ahmadi <peymangeek@gmail.com>
2025-10-22 19:34:57 +0330

    Hero buttons Initialzed

---
+++ components/templates/home/hero/fragments/buttons/elements/about-concepto.tsx
@@ -0,0 +1,9 @@
+import Button from "@/components/ui/button/";
+import { ChevronLeft } from "lucide-react";
+import React from "react";
+
+const AboutConcepto = () => {
+  return <Button color="white" suffixIcon={<ChevronLeft />}>درباره کانسپتو</Button>;
+};
+
+export default AboutConcepto;
---
+++ components/templates/home/hero/fragments/buttons/elements/start-free.tsx
@@ -0,0 +1,15 @@
+import Button from '@/components/ui/button/'
+import React from 'react'
+
+const StartFree = () => {
+  return (
+    <Button
+     color='blue'
+     radius='lg'
+     >
+        رایگان شروع کن!
+    </Button>
+  )
+}
+
+export default StartFree
