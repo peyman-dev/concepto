7d2e2345bf5af2b99eda72d6b1cdcfb7c167ff51
Peyman Ahmadi <peymangeek@gmail.com>
2025-10-22 21:11:57 +0330

    Configured NextConfig for image stuff

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,17 @@
 import type { NextConfig } from "next";
-import utwm from 'unplugin-tailwindcss-mangle/webpack';
+import utwm from "unplugin-tailwindcss-mangle/webpack";
+import daysToSeconds from "./core/helpers/days-to-seconds";
+
+const cacheTTL = daysToSeconds(31) // 31 days = 26,XXX seconds
+
 const nextConfig: NextConfig = {
-/* config options here */
-webpack: (config) => {
-config.plugins.push(utwm());
-return config;
-},
+  /* config options here */
+  webpack: (config) => {
+    config.plugins.push(utwm());
+    return config;
+  },
+  images: {
+    minimumCacheTTL: cacheTTL,
+  },
 };
 export default nextConfig;
