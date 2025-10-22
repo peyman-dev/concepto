844f75b2f8ec0f378762c595e2f2cb0871b2293d
Peyman Ahmadi <peymangeek@gmail.com>
2025-10-22 21:12:44 +0330

    Container got animation

--- components/ui/container.tsx
+++ components/ui/container.tsx
@@ -1,11 +1,16 @@
-import { IContainerProps } from "@/core/lib/types";
+"use client"
+import useViewAnimate from "@/core/hooks/useViewAnimate";
+import { IContainerProps } from "@/core/lib/types/types";
 import clsx from "clsx";
 import React from "react";
 
 const Container = ({ as: Component = "div", children, className }: IContainerProps) => {
-  return <Component className={clsx("container", className)} >
+  const {config,motion} = useViewAnimate()
+  const MotionComponent = motion(Component)
+
+  return <MotionComponent {...config} className={clsx("container", className)} >
     {children}
-  </Component>;
+  </MotionComponent>;
 };
 
 export default Container;
