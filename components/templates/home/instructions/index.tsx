f427d164ebf65738d14396fb748308ce16aefdaf
Peyman Ahmadi <peymangeek@gmail.com>
2025-10-22 21:13:35 +0330

    Constant instruction added + Did a map on UI

--- components/templates/home/instructions/index.tsx
+++ components/templates/home/instructions/index.tsx
@@ -1,7 +1,16 @@
+import Container from "@/components/ui/container";
 import React from "react";
+import Instruction from "./instruction";
+import { IInstructionProps } from "@/core/lib/types/types";
+import instructions from "@/core/constants/instructions";
+
+
 
 const Instructions = () => {
-  return <div>Instructions</div>;
+
+  return <Container as="section" className="grid grid-cols-3 gap-6">
+   {instructions.map((instruction) => <Instruction {...instruction} key={instruction.id}/>)}
+  </Container>;
 };
 
 export default Instructions;
---
+++ core/constants/instructions.ts
@@ -0,0 +1,29 @@
+import { IInstructionProps } from "../lib/types/types";
+
+interface InstructionItemType extends IInstructionProps {
+  id: string | number;
+}
+
+const instructions: InstructionItemType[] = [
+    {
+        id: crypto.randomUUID(),
+        imageSource: "/assets/static/instruction-1.png",
+        label: "شرکت‌های مورد علاقت رو پیدا کن",
+        level: "1"
+    },
+    {
+        id: crypto.randomUUID(),
+        imageSource: "/assets/static/instruction-2.png",
+        label: "کسب و کارهای مناسب رو پیدا کن",
+        level: "2"
+    },
+    {
+        id: crypto.randomUUID(),
+        imageSource: "/assets/static/instruction-3.png",
+        label: "با کسب و کارها ارتباط بگیر",
+        level: "3"
+    },
+    
+]
+
+export default instructions
