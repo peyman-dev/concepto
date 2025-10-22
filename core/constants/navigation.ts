7f53ed18a0a4d64993154ef964905159387b2a6c
Peyman Ahmadi <peymangeek@gmail.com>
2025-10-22 18:56:57 +0330

    Navigation constant added

---
+++ core/constants/navigation.ts
@@ -0,0 +1,29 @@
+const navigation = [
+  {
+    id: crypto.randomUUID(),
+    title: "شرکت ها",
+    href: "/companies",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "فرصت های شغلی",
+    href: "/jobs",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: " اخبار و رویداد ها",
+    href: "/events",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "درباره ما",
+    href: "/about-us",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "تماس با ما",
+    href: "/contact-us",
+  },
+];
+
+export default navigation;
