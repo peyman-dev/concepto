f7c07afa188c6b81152d439e58d2fd9224526e69
Peyman Ahmadi <peymangeek@gmail.com>
2025-10-25 14:01:56 +0330

    Other commits

--- components/templates/home/popular-companies/index.tsx
+++ components/templates/home/popular-companies/index.tsx
@@ -1,10 +1,9 @@
-import Container from '@/components/ui/container'
 import React from 'react'
 
-export default function PopularCompanies() {
+const PopularCompanies = () => {
   return (
-    <section>
-      
-    </section>
+    <div>PopularCompanies</div>
   )
 }
+
+export default PopularCompanies
