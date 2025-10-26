844850374a1e7ceda39d03a2d942eb1e928bc922
Peyman Ahmadi <peymangeek@gmail.com>
2025-10-26 11:27:36 +0330

    Developed Slider Component - W Feature: Slides Arrows

---
+++ components/common/slider/fragments/buttons-provider.tsx
@@ -0,0 +1,46 @@
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import React, { ReactNode } from "react";
+import { tv } from "tailwind-variants";
+
+const ButtonsProvider = ({
+  children,
+  swiper,
+  useArrows,
+}: {
+  children: ReactNode;
+  swiper: any;
+  useArrows?: boolean;
+}) => {
+  const button = tv({
+    base: "absolute z-10 cursor-pointer border border-neutral-100 text-2xl text-black size-[50px]! bg-white flex-center rounded-full top-0 bottom-0 my-auto",
+    variants: {
+      position: {
+        right: "left-[-5%]",
+        left: "right-[-5%]",
+      },
+    },
+  });
+
+  if (useArrows)
+    return (
+      <>
+        <button
+          onClick={() => {
+            swiper?.current?.slideNext()
+          }}
+          className={button({ position: "right" })}
+        >
+          <ChevronLeft className="size-4.5" />
+        </button>
+        {children}
+        <button onClick={() => swiper?.current?.slidePrev()}
+          className={button({ position: "left" })}
+          >
+          <ChevronRight className="size-4.5" />
+        </button>
+      </>
+    );
+  else return null;
+};
+
+export default ButtonsProvider;
--- components/common/slider/index.tsx
+++ components/common/slider/index.tsx
@@ -1,30 +1,31 @@
 "use client";
-import React, { Children } from "react";
-import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import React, { Children, useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { SliderType } from "./lib/slider.type";
 import "swiper/css";
-import ArrowsHandler from "./fragments/arrows-handler";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import Container from "@/components/ui/container";
+import ButtonsProvider from "./fragments/buttons-provider";
 
 const Slider = ({ children, useArrows, ...props }: SliderType) => {
-  const swiper = useSwiper();
+  const swiperRef = useRef<any>(null);
+
 
   return (
     <Container as="section" className="relative">
-           <button className="absolute z-10 cursor-pointer border border-neutral-100 text-2xl text-black size-[50px]! bg-white flex-center rounded-full left-[105%] top-0 bottom-0 my-auto">
-        <ChevronRight className="size-4.5" />
-      </button>
-      <Swiper {...props} className="relative!">
-        <ArrowsHandler hasButtons={useArrows} swiper={swiper}>
+      <ButtonsProvider swiper={swiperRef} useArrows={useArrows}>
+        <Swiper
+          {...props}
+          onSwiper={(swiper) => {
+            swiperRef.current = swiper;
+          }}
+          className="relative"
+        >
           {Children.map(children, (child, i) => (
             <SwiperSlide key={i}>{child}</SwiperSlide>
           ))}
-        </ArrowsHandler>
-      </Swiper>
-      <button className="absolute z-10 cursor-pointer border border-neutral-100 text-2xl text-black size-[50px]! bg-white flex-center rounded-full right-[105%] top-0 bottom-0 my-auto">
-        <ChevronLeft className="size-4.5" />
-      </button>
+        </Swiper>
+      </ButtonsProvider>
     </Container>
   );
 };
