38d2838ad3011db923ba3f1d6855fb25e9c77de1
Peyman Ahmadi <peymangeek@gmail.com>
2025-10-28 13:44:19 +0330

    HeroSection is responsived

--- components/templates/home/hero/index.tsx
+++ components/templates/home/hero/index.tsx
@@ -7,7 +7,7 @@ import Buttons from "./fragments/buttons";
 const HeroSection = () => {
   return (
     <Container as="section" className="flex-col-center gap-8 mt-[96px]">
-      <div className="flex-col-center gap-6">
+      <div className="flex-col-center gap-4 md:gap-6">
         <Title />
         <Description />
       </div>
