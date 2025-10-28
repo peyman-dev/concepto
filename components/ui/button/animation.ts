e20eff2c0dc0d81b1a8dd276c6f422a573b14bce
Peyman Ahmadi <peymangeek@gmail.com>
2025-10-28 13:43:12 +0330

    Button: Animation file has been updated

--- components/ui/button/animation.ts
+++ components/ui/button/animation.ts
@@ -1,3 +1,4 @@
+import { isMobile } from "@/core/hooks/useIsMobile";
 import {
   RIPPLE_FADE_DURATION,
   RIPPLE_SCALE,
@@ -8,14 +9,15 @@ interface IRippleProps {
   ripple: HTMLElement;
   x: number;
   y: number;
+  isIconButton?: boolean
 }
-export default function startRippleAnimation({ ripple, x, y }: IRippleProps) {
+export default function startRippleAnimation({ ripple, x, y, isIconButton }: IRippleProps) {
   const size = RIPPLE_SIZE;
   const halfSize = size / 2;
 
   ripple.style.left = `${x - halfSize}px`;
   ripple.style.top = `${y - halfSize}px`;
-  ripple.style.scale = RIPPLE_SCALE;
+  ripple.style.scale = RIPPLE_SCALE(isIconButton);
   ripple.style.right = "auto";
 
   setTimeout(() => {
